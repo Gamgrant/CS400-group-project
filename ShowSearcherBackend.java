6d7e344bd04dbb4eaefc2c5ea70d7a457a4c94e6
Gamgrant <94784561+Gamgrant@users.noreply.github.com>
2022-03-02 21:59:59 -0600

    Add files via upload

--- ShowSearcherBackend.java
+++ ShowSearcherBackend.java
@@ -1,6 +1,18 @@
+// --== CS400 P1W4. Team App Integration ==--
+// Name: Grant Ovsepyan
+// CSL Username: ovsepyan
+// Email: govsepyan@wisc.edu
+// Lecture #: 004 @4:00pm
+// Notes to Grader: <any optional extra notes to your grader>
+
 import java.util.*;
 import java.util.HashMap;
 
+/*
+ * This class sets the blueprint for the ShowSearcherBackend class. It sets the numberOfShows that
+ * were added to the class; creates the hash table for the year and the title, and set up the toggle
+ * variables for each provider
+ */
 public class ShowSearcherBackend implements IShowSearcherBackend {
 
   private IHashTableSortedSets<Integer, IShow> hashYear;
@@ -12,7 +24,10 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
   private boolean disney;
   private boolean hulu;
 
-
+  /*
+   * this COnstructor creates 2 hash tables and sets all the status providers to true ( no filters
+   * on the providers have been applied yet
+   */
   public ShowSearcherBackend() {
     // create 2 hash tables: one with year of the show as a key
     // and the other with the show title as a key
@@ -27,10 +42,12 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
   }
 
 
-
-  
+  /*
+   * This method adds a show to each hash table. Year hash table receives only entry per movie. The
+   * title is split up by a "space" delimiter, and separate entries are made for each word in the
+   * title Returns nothing
+   */
   @Override
-  
   public void addShow(IShow show1) {
     // add show to hashYear hash table
     int year = show1.getYear();
@@ -45,24 +62,35 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
     numberOfShows++;
   }
 
+  /*
+   * THis method returns the number of shows that have been added to a back end object
+   */
   @Override
   public int getNumberOfShows() {
     return numberOfShows;
   }
 
 
-
+  /*
+   * This method is searching the word in the title of the movies that were added to a back end
+   * object. the list of the shows is returned. If no matches were found, this method will return an
+   * empty array
+   */
   @Override
   public List<IShow> searchByTitleWord(String word) {
     try {
       List<IShow> unfilteredShowsByTitle = hashTitle.get(word);
       return Filter(unfilteredShowsByTitle);
     } catch (NoSuchElementException e) {
-      // just return an empty list if the provided word is not in any of the titles 
+      // just return an empty list if the provided word is not in any of the titles
       return (new ArrayList<>());
     }
   }
 
+  /*
+   * This method is searching the movies by year that were added to a back end. object. the list of
+   * the shows is returned. If no matches were found, this method will return an empty array
+   */
   @Override
   public List<IShow> searchByYear(int year) {
     try {
@@ -75,7 +103,13 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
     }
   }
 
-  private List<IShow> Filter (List<IShow> shows) {
+  /*
+   * This method filters the shows by the provider. It checks which providers were filtered out, and
+   * adds the desired providers to a linked list. Then it checks each show, piped into the method,
+   * and creates a new list of shows that has a matching provider. that list is returned at the end
+   * of the method
+   */
+  private List<IShow> Filter(List<IShow> shows) {
     // put the providers in the list
     List<String> ProvidersAvailable = new LinkedList<String>();
 
@@ -114,7 +148,9 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
   }
 
 
-
+  /*
+   * THis method sets the status of the provider t either true or false
+   */
   @Override
   public void setProviderFilter(String provider, boolean filter) {
     if (provider.equals("Prime Video")) {
@@ -134,6 +170,11 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
     }
   }
 
+
+  /*
+   * This method returns true if the specified provider is not filtered out by the user returns true
+   * if the specified provider is on, false otherwise
+   */
   @Override
   public boolean getProviderFilter(String provider) {
     if ((provider.equals("Prime Video")) && (this.prime == true)) {
@@ -152,7 +193,10 @@ public class ShowSearcherBackend implements IShowSearcherBackend {
   }
 
 
-  // change the boolean of the provider
+  /*
+   * This method changes the status of the provider to the opposite boolean value
+   */
+
   @Override
   public void toggleProviderFilter(String provider) {
 
--- ShowSearcherBackendTester.java
+++ ShowSearcherBackendTester.java
@@ -1,8 +1,19 @@
+// --== CS400 P1W4. Team App Integration ==--
+// Name: Grant Ovsepyan
+// CSL Username: ovsepyan
+// Email: govsepyan@wisc.edu
+// Lecture #: 004 @4:00pm
+// Notes to Grader: <any optional extra notes to your grader>
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ShowSearcherBackendTester {
+  /*
+   * This tester checks if the size of the back end object correctly increments as the shows are
+   * added to a back end object returns true if method is passed, false otherwise
+   */
   public static boolean test1() {
 
     ShowSearcherBackend backend = new ShowSearcherBackend();
@@ -20,8 +31,10 @@ public class ShowSearcherBackendTester {
     return true;
   }
 
-  // by default all of the providers are set to true;
-
+  /*
+   * This tester checks if the get provider correctly works. returns true if method is passed, false
+   * otherwise
+   */
   public static boolean test2() {
     ShowSearcherBackend backend = new ShowSearcherBackend();
     Show show1 = new Show("Title1", 2030, 87, "Disney+");
@@ -47,7 +60,10 @@ public class ShowSearcherBackendTester {
   }
 
 
-
+  /*
+   * This method checks if the setProvider can set the correct boolean value of the toggle variables
+   * returns true if method is passed, false otherwise
+   */
   public static boolean test3() {
 
     ShowSearcherBackend Empty_Backend = new ShowSearcherBackend();
@@ -73,9 +89,12 @@ public class ShowSearcherBackendTester {
     return true;
   }
 
-  // test provider
 
 
+  /*
+   * This method tests if none of the shows are filtered ou tby default returns true if method is
+   * passed, false otherwise
+   */
   public static boolean test4() {
     ShowSearcherBackend backend = new ShowSearcherBackend();
     // by default, all the providers are set to 'true' value
@@ -95,7 +114,10 @@ public class ShowSearcherBackendTester {
     return true;
   }
 
-  // test toggle providers
+  /*
+   * This method toggles all the providers, and sets all of their status variable to false returns
+   * true if method is passed, false otherwise
+   */
   public static boolean test5() {
     ShowSearcherBackend backend = new ShowSearcherBackend();
 
@@ -123,7 +145,11 @@ public class ShowSearcherBackendTester {
     return true;
   }
 
-  // search by year
+  /*
+   * This method test the search method by year. it checks the size of the returned sorted list and
+   * checks if the value inside the list actually equals to the show object. returns true if method
+   * is passed, false otherwise
+   */
   public static boolean test6() {
     ShowSearcherBackend backend = new ShowSearcherBackend();
     Show show1 = new Show("Title1", 2030, 87, "Disney+");
@@ -133,48 +159,57 @@ public class ShowSearcherBackendTester {
     backend.addShow(show1);
     backend.addShow(show2);
     backend.addShow(show3);
-    
-    
-    if(backend.searchByYear(2030).size() != 1) {
+
+
+    if (backend.searchByYear(2030).size() != 1) {
       return false;
     }
-    
+
     if (!backend.searchByYear(2030).get(0).equals(show1)) {
       return false;
     }
 
-     return true;
+    return true;
+  }
+
+
+  /*
+   * This method test the search method by Title. it checks the size of the returned sorted list and
+   * checks if the value inside the list actually equals to the show object. returns true if method
+   * is passed, false otherwise
+   */
+  public static boolean test7() {
+    ShowSearcherBackend backend = new ShowSearcherBackend();
+    Show show1 = new Show("Title Joe", 2030, 87, "Disney+");
+    Show show2 = new Show("Title Max", 1999, 19, "Hulu");
+    Show show3 = new Show("Title My Mom", 1459, 21, "Netflix");
+
+    backend.addShow(show1);
+    backend.addShow(show2);
+    backend.addShow(show3);
+
+
+    if (backend.searchByTitleWord("Max").size() != 1) {
+      return false;
     }
 
-   
-  // search by title
+    if (!backend.searchByTitleWord("Mom").get(0).equals(show3)) {
+      return false;
+    }
 
-  public static boolean test7() {
-      ShowSearcherBackend backend = new ShowSearcherBackend();
-      Show show1 = new Show("Title Joe", 2030, 87, "Disney+");
-      Show show2 = new Show("Title Max", 1999, 19, "Hulu");
-      Show show3 = new Show("Title My Mom", 1459, 21, "Netflix");
+    return true;
+  }
 
-      backend.addShow(show1);
-      backend.addShow(show2);
-      backend.addShow(show3);
-      
-      
-      if(backend.searchByTitleWord("Max").size() != 1) {
-        return false;
-      }
-      
-      if (!backend.searchByTitleWord("Mom").get(0).equals(show3)) {
-        return false;
-      }
 
-       return true;
-      }
-  
-
-  // search by year filtering the shows on Netflix
+  /*
+   * This method test the search method by year. it checks the size of the returned sorted list and
+   * checks if the value inside the list actually equals to the show object. Then it sets the filter
+   * on all the providers except for Netflix, and checks that only one show is returned after the
+   * search by year with the filter on the provider is applied returns true if method is passed,
+   * false otherwise
+   */
   public static boolean test8() {
-    
+
     ShowSearcherBackend backend = new ShowSearcherBackend();
     Show show1 = new Show("Title1", 2030, 87, "Disney+");
     Show show2 = new Show("Title2", 1192, 19, "Hulu");
@@ -183,9 +218,9 @@ public class ShowSearcherBackendTester {
     backend.addShow(show1);
     backend.addShow(show2);
     backend.addShow(show3);
-    
-    
-    if(backend.searchByYear(2030).size() != 2) {
+
+
+    if (backend.searchByYear(2030).size() != 2) {
       return false;
     }
     // now set Netflix as the only provider
@@ -198,11 +233,16 @@ public class ShowSearcherBackendTester {
     if (backend.searchByYear(2030).size() != 1) {
       return false;
     }
-   
-     return true;
-    }
-  // search by title filtering the shows on Hulu
 
+    return true;
+  }
+
+  /*
+   * This method test the search method by Title. it checks the size of the returned sorted list and
+   * checks if the value inside the list actually equals to the show object. Then it apples filter
+   * on the provider (enabling only Netflix) and checks the number of the listed filtered shows.
+   * returns true if method is passed, false otherwise
+   */
   public static boolean test9() {
     ShowSearcherBackend backend = new ShowSearcherBackend();
     Show show1 = new Show("Get rich or die trying", 2019, 87, "Hulu");
@@ -212,9 +252,9 @@ public class ShowSearcherBackendTester {
     backend.addShow(show1);
     backend.addShow(show2);
     backend.addShow(show3);
-    
-    
-    if(backend.searchByTitleWord("trying").size() != 2) {
+
+
+    if (backend.searchByTitleWord("trying").size() != 2) {
       return false;
     }
     // now set Netflix as the only provider
@@ -224,13 +264,50 @@ public class ShowSearcherBackendTester {
     backend.setProviderFilter("Netflix", true);
 
 
-    if(backend.searchByTitleWord("trying").size() != 1) {
+    if (backend.searchByTitleWord("trying").size() != 1) {
       return false;
     }
-   
+
     return true;
   }
 
+  /*
+   * test#1. testing Front end developer's code. checks that the correct greenings are displayed on
+   * the screen.
+   * 
+   */
+  public static boolean test10() {
+    
+    ShowSearcherBackend backend = new ShowSearcherBackend();
+    Show show1 = new Show("Get rich or die trying", 2019, 87, "Hulu");
+    Show show2 = new Show("Stinky Badger", 1192, 19, "Netflix");
+    Show show3 = new Show("Get rich or die trying", 2030, 21, "Netflix");
+
+    backend.addShow(show1);
+    backend.addShow(show2);
+    backend.addShow(show3);
+    
+    ShowSearcherFrontend testing = new ShowSearcherFrontend(backend);
+    
+    String returningString = testing.outputHelper();
+    if (!returningString.startsWith("Welcome to the Show Searcher App!")) {
+    return false;
+  } else
+    return true;
+  }
+
+  /*
+   * test#2. testing Front end developer's code
+   */
+  public static boolean test11() {
+    return false;
+  }
+
+
+
+  /*
+   * This main method runs all the tests and prints the outcome of each test
+   */
   public static void main(String[] args) {
     System.out.println("Test 1 passed: " + test1());
     System.out.println("Test 2 passed: " + test2());
@@ -241,5 +318,8 @@ public class ShowSearcherBackendTester {
     System.out.println("Test 7 passed: " + test7());
     System.out.println("Test 8 passed: " + test8());
     System.out.println("Test 9 passed: " + test9());
+    System.out.println("Test 10 passed: " + test10());
+    System.out.println("Test 11 passed: " + test11());
+
   }
 }
